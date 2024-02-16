a3d5e4fdfec282660a1547ccfe00d7f289630264
Pietro <pietrovb25@gmail.com>
2024-02-16 20:19:32 -0300

    Add project files.

---
+++ SuperHeroAPI/SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHeroAPI.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class SuperHeroController: ControllerBase
+{
+    [HttpGet]
+
+}
