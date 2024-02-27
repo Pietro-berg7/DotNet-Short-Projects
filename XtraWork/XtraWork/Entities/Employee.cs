bcae61698a2c72c6e1c57a48ada4eeacc468be41
Pietro <pietrovb25@gmail.com>
2024-02-27 19:49:36 -0300

    feat: add entities title and employee

---
+++ XtraWork/XtraWork/Entities/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace XtraWork.Entities;
+
+public class Employee
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public DateTime BirthDate { get; set; }
+    public string Gender { get; set; } = string.Empty;
+    public Title Title { get; set; } = new Title();
+    public Guid TitleId { get; set; }
+}
